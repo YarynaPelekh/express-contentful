6057854060d9be37f3025edbcc3315e64f82188c
Yaryna Pelekh <yaryna.pelekh@hebronsoft.com>
2024-01-27 12:37:35 +0200

    add ts

---
+++ app.ts
@@ -0,0 +1,28 @@
+// const express = require("express");
+import express, { Request, Response } from "express";
+const cf = require("./services/cf");
+const contentful = require("contentful");
+const cors = require("cors");
+
+const app = express();
+
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("Get request  (with TS)");
+  res.send(
+    "Get request (with TS)aaaaaa sdfgb sfdgbsfgnkn ;kjnljhbdfv adflkvjn"
+  );
+});
+
+const booksRouter = require("./routes/books");
+const authorsRouter = require("./routes/authors");
+
+app.use("/books", booksRouter);
+app.use("/authors", authorsRouter);
+
+app.listen("3009");
